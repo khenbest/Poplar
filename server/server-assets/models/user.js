86f7cd150e5a62c9f765d1a4bc35cb48dd261ac5
Mark Ohnsman <mark.ohnsman@gmail.com>
2019-03-10 22:56:39 -0600

    Updated PDF

--- server/server-assets/models/user.js
+++ server/server-assets/models/user.js
@@ -11,21 +11,20 @@ let schema = new Schema({
   name: { type: String, required: true },
   //every email must be unique on the database
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  created: { type: Number, required: true, default: Date.now() }
-})
+  hash: { type: String, required: true }
+}, { timestamps: true })
 
 
 //THESE TWO METHODS CAN BE COPIED FOR ALL USER SCHEMA'S
 
 //statics are used to create Model methods
-schema.statics.generateHash = function (password) {
-  return bcrypt.hashSync(password, SALT)
+schema.statics.generateHash = function (hash) {
+  return bcrypt.hashSync(hash, SALT)
 }
 
 //schema.methods are used to add a method to a Model instance
 schema.methods.validatePassword = function (password) {
-  return bcrypt.compare(password, this.password)
+  return bcrypt.compare(password, this.hash)
 }
 
 module.exports = mongoose.model('User', schema)
