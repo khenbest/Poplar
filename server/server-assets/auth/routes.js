95d9199e1d66d1e240c854c27bbf0c1893046a2d
Darryl Kilzer Jr <darrylkilzer@gmail.com>
2019-03-18 17:06:13 -0600

    wrap in then, duh

--- server/server-assets/auth/routes.js
+++ server/server-assets/auth/routes.js
@@ -46,13 +46,14 @@ router.post('/auth/login', (req, res) => {
           if(!valid){
             return res.status(400).send(loginError)
           }
-        })
+        
         
       
       //ALWAYS REMOVE THE PASSWORD FROM THE USER OBJECT
       delete user._doc.hash
       req.session.uid = user._id
       res.send(user)
+    })
     }).catch(err => {
       res.status(400).send(loginError)
     })
