d101ac47391a27acb8bef24727c0e27966a6bdab
Caleb Sherfick <calebsherfick@gmail.com>
2019-03-19 15:27:04 -0600

    made chatroom button

--- client/src/views/PostDetails.vue
+++ client/src/views/PostDetails.vue
@@ -1,18 +1,17 @@
 <template>
    <div class="postDetails">
-      
    </div>
 </template>
 
 <script>
-export default {
-   name: "postDetails",
-   props: [],
-   data() {
-      return {}
-   },
-   computed: {},
-   methods: {},
-   components: {}
-}
+   export default {
+      name: "postDetails",
+      props: [],
+      data() {
+         return {}
+      },
+      computed: {},
+      methods: {},
+      components: {}
+   }
 </script>
