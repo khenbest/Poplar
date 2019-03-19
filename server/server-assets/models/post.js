96315122ba70ca99082dd4c10f9513219e63bf92
Kenny Henbest <lekenbest@gmail.com>
2019-03-19 14:17:34 -0600

    backend start

--- server/server-assets/models/post.js
+++ server/server-assets/models/post.js
@@ -1,32 +1,31 @@
 let mongoose = require('mongoose')
 let Schema = mongoose.Schema
 let ObjectId = Schema.Types.ObjectId
-let schemaName = 'Board'
+let schemaName = 'Post'
 
-let Lists = require('./list')
+// let Lists = require('./list')
 
 
 let schema = new Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
   authorId: { type: ObjectId, ref: 'User', required: true },
-  imgUrl: { type: String, },
-  optionOne: { type: Number },
-  optionTwo: { type: Number }
+  imgUrl: { type: String, required: true },
+  votes: {},
+  tags: { type: String, enum: ["music", "sports", "fashion", "food", "movies"] }
 }, { timestamps: true })
 
 //CASCADE ON DELETE
 
-schema.pre('remove', function (next) {
-  //lets find all the lists and remove them
-  this._id //THIS IS THE BOARD
-  Promise.all([
-    //Tasks.deleteMany({ boardId: this._id }),
-    Lists.deleteMany({ boardId: this._id })
-  ])
-    .then(() => next())
-    .catch(err => next(err))
-})
+// schema.pre('remove', function (next) {
+//   //lets find all the lists and remove them
+//   this._id //THIS IS THE BOARD
+//   Promise.all([
+//     //Tasks.deleteMany({ boardId: this._id }),
+//     Lists.deleteMany({ boardId: this._id })
+//   ])
+//     .then(() => next())
+//     .catch(err => next(err))
+// })
 
 
 
