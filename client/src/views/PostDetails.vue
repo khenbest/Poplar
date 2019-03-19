ca0c2e4c32f7e2d33d31b17ce681e1a469da93b1
Dominic Hulsey <dominic.hulsey.dh@gmail.com>
2019-03-19 15:28:18 -0600

    postDetails chatroom update

--- client/src/views/PostDetails.vue
+++ client/src/views/PostDetails.vue
@@ -1,17 +1,10 @@
 <template>
    <div class="postDetails">
-<<<<<<< HEAD
-      <chatroom></chatroom>
-=======
->>>>>>> d101ac47391a27acb8bef24727c0e27966a6bdab
    </div>
 </template>
 
 <script>
-<<<<<<< HEAD
    import Chatroom from "./Chatroom.vue"
-=======
->>>>>>> d101ac47391a27acb8bef24727c0e27966a6bdab
    export default {
       name: "postDetails",
       props: [],
@@ -20,12 +13,8 @@
       },
       computed: {},
       methods: {},
-<<<<<<< HEAD
       components: {
          Chatroom
       }
-=======
-      components: {}
->>>>>>> d101ac47391a27acb8bef24727c0e27966a6bdab
    }
 </script>
