21f9c298527b041fcb98a5f43d6f2a2cad2b54e4
Dominic Hulsey <dominic.hulsey.dh@gmail.com>
2019-03-20 14:24:34 -0600

    merge conflicts

--- client/src/router.js
+++ client/src/router.js
@@ -6,13 +6,8 @@ import Posts from './views/Posts.vue'
 import PostDetails from './views/PostDetails.vue'
 // @ts-ignore
 import Login from './views/Login.vue'
-<<<<<<< HEAD
 import makePost from './components/makePost.vue'
 
-=======
-import Profile from './views/Profile.vue'
-import MyProfile from './views/MyProfile.vue'
->>>>>>> f425957a7bca2f6661ec18adc5e61f225ba5dab9
 Vue.use(Router)
 
 export default new Router({
