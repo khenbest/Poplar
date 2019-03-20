d523699b9702c4210eff30297fd72a0846a21b4e
Caleb Sherfick <calebsherfick@gmail.com>
2019-03-20 14:38:12 -0600

    fixed small issues

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -18,10 +18,10 @@
       </div>
       <div class="row mb-2 mt-2 justify-content-between">
         <div class="col">
-          <button class="vote yes" @click="castVote(post._id, 'yes')">yes</button>
+          <button class="vote yes" @click="castVote('yes')">yes</button>
         </div>
         <div class="col">
-          <button class="vote no" @click="castVote(post._id, 'no')">no</button>
+          <button class="vote no" @click="castVote('no')">no</button>
         </div>
       </div>
       <div class="row mb-2">
@@ -36,111 +36,111 @@
 </template>
 
 <script>
-import Moment from "moment";
+  import Moment from "moment";
 
-export default {
-  name: "post",
-  props: [],
-  data() {
-    return {};
-  },
-  computed: {
-    posts() {
-      return this.$store.state.posts;
-    }
-  },
-  methods: {
-    deletePost(postId) {
-      this.$store.dispatch("deletePost", postId);
+  export default {
+    name: "post",
+    props: [],
+    data() {
+      return {};
     },
-    castVote(vote) {
-      debugger;
-      let postId = this.post.id;
+    computed: {
+      posts() {
+        return this.$store.state.posts;
+      }
+    },
+    methods: {
+      deletePost(postId) {
+        this.$store.dispatch("deletePost", postId);
+      },
+      castVote(vote) {
+        debugger;
+        let postId = this.post.id;
 
-      console.log(postId);
-      this.$store.dispatch("castVote", {
-        endpoint: "posts/${postId}"
-      });
+        console.log(postId);
+        this.$store.dispatch("castVote", {
+          endpoint: "posts/${postId}"
+        });
+      }
+    },
+    components: {},
+    filters: {
+      formatTime(date) {
+        return Moment(String(date))
+          .startOf("hour")
+          .fromNow();
+      }
     }
-  },
-  components: {},
-  filters: {
-    formatTime(date) {
-      return Moment(String(date))
-        .startOf("hour")
-        .fromNow();
-    }
-  }
-};
+  };
 </script>
 
 
 <style scoped>
-.username {
-  color: #a0b5c5;
-  font-family: "Amatic SC", cursive;
-  margin-bottom: -0.2em;
-}
+  .username {
+    color: #a0b5c5;
+    font-family: "Amatic SC", cursive;
+    margin-bottom: -0.2em;
+  }
 
-.title {
-  color: #3d6ea0;
-  font-family: "Patrick Hand SC", cursive;
-  margin-bottom: -0.05em;
-}
+  .title {
+    color: #3d6ea0;
+    font-family: "Patrick Hand SC", cursive;
+    margin-bottom: -0.05em;
+  }
 
-img {
-  max-width: 100%;
-  margin-left: -15px;
-  margin-right: -15px;
-}
+  img {
+    max-width: 100%;
+    margin-left: -15px;
+    margin-right: -15px;
+  }
 
-.yes {
-  background-color: #95c701;
-}
+  .yes {
+    background-color: #95c701;
+  }
 
-.no {
-  background-color: #fe3231;
-}
+  .no {
+    background-color: #fe3231;
+  }
 
-.vote {
-  border: none;
-  color: white;
-  font-size: 1.5em;
-  min-width: 5em;
-  min-height: 1em;
-  font-family: "Kalam", cursive;
-}
+  .vote {
+    border: none;
+    color: white;
+    font-size: 1.5em;
+    min-width: 5em;
+    min-height: 1em;
+    font-family: "Kalam", cursive;
+  }
 
-.timestamp {
-  color: #3d6ea0;
-  font-size: 1em;
-}
+  .timestamp {
+    color: #3d6ea0;
+    font-size: 1em;
+  }
 
-.filters {
-  color: #c2c2c3;
-}
+  .filters {
+    color: #c2c2c3;
+  }
 
-.filters:active {
-  color: #3d6ea0;
-}
+  .filters:active {
+    color: #3d6ea0;
+  }
 
-.fas:hover {
-  cursor: pointer;
-}
+  .fas:hover {
+    cursor: pointer;
+  }
 
-/* TEMPORARY STYLING FOR TEMPORARY CHATROOM BUTTON */
-.chatroom {
-  border: none;
-  background-color: #3d6ea0;
-  color: white;
-  border-radius: 25px;
-  font-size: 1.5em;
-  min-width: 5em;
-  min-height: 1em;
-  font-family: "Kalam", cursive;
-}
+  /* TEMPORARY STYLING FOR TEMPORARY CHATROOM BUTTON */
+  .chatroom {
+    border: none;
+    background-color: #3d6ea0;
+    color: white;
+    border-radius: 25px;
+    font-size: 1.5em;
+    min-width: 5em;
+    min-height: 1em;
+    font-family: "Kalam", cursive;
+  }
 
-/* font-family: 'Amatic SC', cursive;
+  /* font-family: 'Amatic SC', cursive;
 font-family: 'Patrick Hand SC', cursive;
 font-family: 'Dekko', cursive;
 font-family: 'Itim', cursive;
