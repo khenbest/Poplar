fe3e992af1ff9811b18cca56e7b88405b7622644
Kenny Henbest <lekenbest@gmail.com>
2019-03-20 13:46:12 -0600

    myposts

---
+++ client/src/views/MyProfile.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="myProfile ">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "myProfile",
+        props: [],
+        data() {
+            return {}
+        },
+        mounted() {
+            this.$store.dispatch("getPosts", true); //with a second argument passed in that is truthy this will get just your posts
+
+        },
+        computed: {},
+        methods: {},
+        components: {}
+    }
+</script>
