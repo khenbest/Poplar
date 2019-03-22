723506265c4eefae3499102d0f2fce9f8d572029
Kenny Henbest <lekenbest@gmail.com>
2019-03-22 09:45:13 -0600

    curly

--- client/src/views/MyProfile.vue
+++ client/src/views/MyProfile.vue
@@ -128,35 +128,35 @@
             participated() {
                 return this.$store.state.user.participated
             },
-            methods: {
-                addPost() {
-                    this.$store.dispatch("addPost", this.newPost);
-                    event.target.reset()
-                },
-                deletePost(postId) {
-                    this.$store.dispatch("deletePost", postId);
-                },
-                chatroom() {
-                    this.$router.push({ name: 'postDetails' })
-                },
-                allPosts() {
-                    this.$router.push({ path: '/' });
-                },
-                myProfile() {
-                    this.$router.push({ path: '/myProfile' })
-                },
+        },
+        methods: {
+            addPost() {
+                this.$store.dispatch("addPost", this.newPost);
+                event.target.reset()
+            },
+            deletePost(postId) {
+                this.$store.dispatch("deletePost", postId);
+            },
+            chatroom() {
+                this.$router.push({ name: 'postDetails' })
+            },
+            allPosts() {
+                this.$router.push({ path: '/' });
+            },
+            myProfile() {
+                this.$router.push({ path: '/myProfile' })
+            },
 
+        },
+        components: {
+            Chatroom
+        },
+        filters: {
+            formatTime(date) {
+                return Moment(String(date)).startOf('hour').fromNow();
             },
-            components: {
-                Chatroom
-            },
-            filters: {
-                formatTime(date) {
-                    return Moment(String(date)).startOf('hour').fromNow();
-                },
-                formatTime2(date) {
-                    return Moment(String(date)).format('MMMM Do, YYYY')
-                }
+            formatTime2(date) {
+                return Moment(String(date)).format('MMMM Do, YYYY')
             }
         }
     }
