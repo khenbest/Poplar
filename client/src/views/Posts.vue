475295c007096b271dd1a8cd5bc4e26148ed3880
Kenny Henbest <lekenbest@gmail.com>
2019-04-25 10:45:30 -0600

    kenny

--- client/src/views/Posts.vue
+++ client/src/views/Posts.vue
@@ -45,17 +45,7 @@
           <a class="dropdown-item text-dark" @click=" filterbyTag('meme-culture')">Meme-Culture</a>
         </div>
       </div>
-      <div class="btn-group btn-group-toggle" data-toggle="buttons">
-        <label class="btn btn-outline-primary" active>
-          <input type="radio" name="option" v-model="filter" id="option1" @change="activity"> Most Poplar
-        </label>
-        <label class="btn btn-outline-primary" active>
-          <input type="radio" name="option" v-model="filter" id="option2" @change="reset"> Newest
-        </label>
-        <label class="btn btn-outline-primary" active>
-          <input type="radio" name="option" v-model="filter" id="option3"> Oldest
-        </label>
-      </div>
+
       <form v-if="showForm" @submit.prevent="filterbyUser">
         <input type="text" v-model="username" placeholder="type username here" />
         <button class="btn btn-outline-primary" type="submit">Search</button>
