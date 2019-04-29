bef58c16f5daa19e4ae9706433d6d00fc428d1b7
Caleb Sherfick <calebsherfick@gmail.com>
2019-04-28 22:54:35 -0600

    .gitignore session.js

--- server/server-assets/auth/session.js
+++ server/server-assets/auth/session.js
@@ -12,7 +12,7 @@ store.on("error", function (err) {
 
 // @ts-ignore
 var session = expressSession({
-  secret: "poplar",
+  secret: "poplarMadeBy",
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7 * 52 * 2,
   },
